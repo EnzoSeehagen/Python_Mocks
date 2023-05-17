55c773eea2a6550cadd8dd71d815a2ca9becddd1
Enzo Seehagen <enzovonseehagen@gmail.com>
2023-05-17 20:30:26 -0300

    Utilizando Mock para simular envio de e-mail

--- mock_test/enviador_de_spam.py
+++ mock_test/enviador_de_spam.py
@@ -1,7 +1,25 @@
-class EnviadorDeSpam :
-    def __init__(self, destinatarios = None):
+class CanalEmail:
+    def enviar(self, destinatario, mensagem):
+        nome = destinatario.split("@")[0].capitalize()
+        print("E-mail enviado erroneamente")
+        mensagem_final = f'{mensagem} para {nome}'
+        return destinatario, mensagem_final
+
+
+class CanalMock:
+    def enviar(self, destinatario, mensagem):
+        return "enzo@gmail.com", "Spam enviado para Enzo"
+
+
+class EnviadorDeSpam:
+    def __init__(self, destinatarios=None):
         self.destinatarios = destinatarios if destinatarios else []
+        self.canais_de_envio = [CanalEmail()]
 
     def adicionar_destinatarios(self, destinatario):
         self.destinatarios.append(destinatario)
 
+    def enviar_spam(self, mensagem):
+        for destinatario in self.destinatarios:
+            for canal in self.canais_de_envio:
+                yield canal.enviar(destinatario, mensagem)
--- mock_test/test_enviador_de_spam.py
+++ mock_test/test_enviador_de_spam.py
@@ -1,4 +1,4 @@
-from mock_test.enviador_de_spam import EnviadorDeSpam
+from mock_test.enviador_de_spam import EnviadorDeSpam, CanalEmail, CanalMock
 
 
 def test_enviador_de_span():
@@ -12,3 +12,11 @@ def test_enviador_possui_destinatarios():
     enviador.adicionar_destinatarios("messias@gmail.com")
     assert enviador.destinatarios == ["enzo@gmail.com", "messias@gmail.com"]
 
+
+def test_envio_de_spam():
+    enviador = EnviadorDeSpam(["enzo@gmail.com"])
+    enviador.canais_de_envio=[CanalMock()]
+
+    assert list(enviador.enviar_spam("Spam enviado")) == [
+        ("enzo@gmail.com", "Spam enviado para Enzo")
+    ]
